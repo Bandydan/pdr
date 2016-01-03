43cfa4092958c7955c02817b0805b67dd9db36b7
VetalBeliy <vbelokurenko@gmail.com>
2016-01-03 15:42:41 +0200

    Autoload controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'pdr/index.php/examples/index';
+$route['default_controller'] = 'examples';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
