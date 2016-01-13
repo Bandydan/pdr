93f74a5bc61fba23829b9036d0e140d9793ba465
skvak <a.kvak17@gmail.com>
2016-01-14 00:22:26 +0200

    add simple registration with session

---
+++ application/views/user/register/register_success.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php echo '<meta http-equiv="Refresh" content="5; url=http://pdr/index.php/login">';?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h2>Спасибо за регистрацию!</h2>
+			</div>
+			<p>Сейчас вы автоматически будете перенаправлены на страницу авторизации.</p>
+		</div>
+	</div><!-- .row -->
+</div><!-- .container -->
