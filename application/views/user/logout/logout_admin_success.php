557371edbf9331fec830f62786ab7f6f3cf5fa49
skvak <a.kvak17@gmail.com>
2016-01-19 16:34:11 +0200

    add view for admin logout

---
+++ application/views/user/logout/logout_admin_success.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php echo '<meta http-equiv="Refresh" content="2; url=http://pdr/index.php/admin">';?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>Спасибо за то что были с нами!</h1>
+			</div>
+			<p>Сеанс <?= $login;?> успешно завершен.</p>
+		</div>
+	</div><!-- .row -->
+</div><!-- .container -->
