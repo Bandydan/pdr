c1f54245beb2b6a0ca62a78a69bf9ffe0ce22f41
skvak <a.kvak17@gmail.com>
2016-01-19 16:38:51 +0200

    some corrections in logout view

--- application/views/user/logout/logout_success.php
+++ application/views/user/logout/logout_success.php
@@ -3,9 +3,9 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="page-header">
-				<h1>Logout success!</h1>
+				<h1>Выход выполнен!</h1>
 			</div>
-			<p>You are now logged out.</p>
+			<p>Сеанс <?= $login;?> успешно завершен.</p>
 		</div>
 	</div><!-- .row -->
 </div><!-- .container -->
