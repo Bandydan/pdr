c49c2dfb32dd1dcdd6a8acf9d596632e936a65d8
skvak <a.kvak17@gmail.com>
2016-01-26 22:14:27 +0200

    some corrections in views

--- application/views/user/login/login_admin_success.php
+++ application/views/user/login/login_admin_success.php
@@ -1,10 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<?php echo '<meta http-equiv="Refresh" content="5; url=http://pdr/admin">';?>
+<?php echo '<meta http-equiv="Refresh" content="2; url=http://pdr/admin">';?>
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="page-header">
-				<h2>Привет, <?= $_SESSION['login']; ?>!</h2>
+				<h2>Привет, <?= $this->session->userdata('login'); ?>!</h2>
 			</div>
 			<p>Вы успешно авторизованы!</p>
 		</div>
