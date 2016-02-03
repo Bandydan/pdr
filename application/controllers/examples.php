55d0848b4011c915de8d3e806a2f0d50e581b816
Alona <alenija23@gmail.com>
2016-02-03 18:26:15 +0200

    Controllers + roots

--- application/controllers/examples.php
+++ application/controllers/examples.php
@@ -20,4 +20,51 @@ class Examples extends CI_Controller {
         $this->load->view('footer', $data);
 
     }
+
+        public function paginate($offset='') 
+       { 
+            $limit = 3;
+            // $limit - кол-во запрашиваемых записей 
+            // $offset - смещение, т.е. с какой записи начинать выборку 
+            
+            $this->load->model('examples_model'); 
+
+            //$data['examples'] = $this->examples_model->get_examples_all();
+            
+            $data['examples'] = $this->examples_model->get_scrolling($limit,$offset); 
+         
+            $this->load->view('header', $data); 
+            $this->load->view('examples_view', $data); 
+            $this->load->view('footer', $data);           
+        } 
+
+    public function works(){        
+
+        $limit = 3;
+        $offset = $_POST['startFrom'];       
+
+        $this->load->model('examples_model');            
+        $data['examples'] = $this->examples_model->get_scrolling($limit,$offset); 
+
+        //var_dump($data['scrolling']);
+
+        echo json_encode($data['examples']);
+        //echo $data;
+        //echo json_encode($data);
+        die(); //чтоб лишнего не передавал, а именно HTML 
+    }
+
+    public function config_arr(){
+        $config['categories'] = array(
+            'pdr' => 'Беспокрасочное удаление вмятин',
+            'pol' => 'Полировка авто', 
+            'brn' => 'Бронирование фар',
+            'chm' => 'Химчистка салона',
+            'gls' => 'Ремонт лобового стекла',
+            'atk' => 'Антикорозийная обработка',
+            'pnt' => 'Покраска авто'
+        );
+
+        echo json_encode($config['categories']);
+    }
 }
