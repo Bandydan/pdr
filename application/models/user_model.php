2725ef5e9f046b5fe5bdd993be70c85f50f51929
Daniil Marynich <bandydan@gmail.com>
2016-02-24 18:37:05 +0200

    small auto refactoring

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,38 +3,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * User_model class.
- * 
+ *
  * @category Database
  * @package	Pdr
  * @subpackage Pdr
  * @author Dream Team <dreamteam@gmail.com>
- * @license private 	
+ * @license private
  * @link https://github.com/Bandydan/pdr
  */
 class User_model extends CI_Model {
 
 	/**
 	 * Almost empty constructor
-	 * 
+	 *
 	 * @return void
 	 */
-	public function __construct() 
+	public function __construct()
 	{
 		parent::__construct();
 		$this->load->database();
 	}
-	
+
 	/**
 	 * Method creates user by given data.
-	 * 
+	 *
 	 * @param array $data - array of incoming user data
-	 * 
+	 *
 	 * @return bool true on success, false on failure
 	 */
-	public function create_user($data) 
+	public function create_user($data)
 	{
 		$car = NULL;
-		if ($data['ManufactureName'] !== '--') 
+		if ($data['ManufactureName'] !== '--')
 		{
 			$car = $this->_get_car_id($data);
 		}
@@ -53,22 +53,22 @@ class User_model extends CI_Model {
 			'user_rights'   => $this->config->item('user_rights'),
 			'password'   	=> $this->_hash_password($pass),
 		);
-		
+
 		$this->db->insert('authorization', $data1);
-		
+
 		return TRUE;
-	}	
-	
+	}
+
 	/**
 	 * @access private
 	 * Method gets id of given auto by its data
 	 * TODO:
 	 * @deprecated METHOD SHOULD BE REMOVED, ID SHOULD COME FROM THE FORM!!!
-	 * @param array $data 
+	 * @param array $data
 	 * @return integer
-	 * 
+	 *
 	 */
-	private function _get_car_id($data) 
+	private function _get_car_id($data)
 	{
 		$this->db->select('id');
 		$this->db->from('avto');
@@ -88,31 +88,31 @@ class User_model extends CI_Model {
 // resolve_user_login and _verify_password_hash functions.
 	/**
 	 * Gets user password from DB and compares it with the given one.
-	 * 
+	 *
 	 * @access public
 	 * @param mixed $login    user login
 	 * @param mixed $password user password
 	 * @return bool true on success, false on failure
 	 */
-	public function resolve_user_login($login, $password) 
+	public function resolve_user_login($login, $password)
 	{
 		$this->db->select('authorization.password as password');
 		$this->db->from('authorization');
 		$this->db->join('users', 'authorization.user_id = users.id', 'inner');
 		$this->db->where('users.login', $login);
 		$hash = $this->db->get()->row('password');
-		
+
 		return $this->_verify_password_hash($password, $hash);
 	}
-	
+
 	/**
 	 * get_user_id_from_username function.
-	 * 
+	 *
 	 * @access public
 	 * @param mixed $login - Login
 	 * @return integer - the user id
 	 */
-	public function get_user_id_from_username($login) 
+	public function get_user_id_from_username($login)
 	{
 		$this->db->select('id');
 		$this->db->from('users');
@@ -120,14 +120,14 @@ class User_model extends CI_Model {
 
 		return $this->db->get()->row('id');
 	}
-	
+
 	/**
 	 * get_user function.
-	 * 
+	 *
 	 * @param integer $user_id - Id of user
 	 * @return object the user object
 	 */
-	public function get_user($user_id) 
+	public function get_user($user_id)
 	{
 		$this->db->from('users');
 		$this->db->where('id', $user_id);
@@ -151,28 +151,28 @@ class User_model extends CI_Model {
 //I suggest changing md5 to http://php.net/manual/ru/function.hash.php with sha256 and use its part (substr) to compare
 	/**
 	 * hash_password function.
-	 * 
+	 *
 	 * @access private
 	 * @param mixed $password - password
 	 * @return string|bool could be a string on success, or bool false on failure
 	 */
-	private function _hash_password($password) 
+	private function _hash_password($password)
 	{
 		return md5($password);
 	}
-	
+
 	/**
 	 * verify_password_hash function.
-	 * 
+	 *
 	 * @access private
 	 * @param mixed $password - password
 	 * @param mixed $hash     - hash
 	 * @return bool
 	 */
-	private function _verify_password_hash($password, $hash) 
+	private function _verify_password_hash($password, $hash)
 	{
 		return $this->_hash_password($password) === $hash;
-	}	
+	}
 
 	/**
 	 * Method saves user session additional data to DB
@@ -193,7 +193,7 @@ class User_model extends CI_Model {
 
 	/**
 	 * Method retrieves user session additional data from DB
-	 * @param string $session_id 
+	 * @param string $session_id
 	 * @return array of data
 	 */
 	public function get_user_session($session_id)
@@ -205,15 +205,15 @@ class User_model extends CI_Model {
 
 	/**
 	 * Method gets array of users from DB
-	 * 
+	 *
 	 * @return query result array
 	 */
 	public function get_users()
 	{
 
 		//I suggest moving user fields to array and then to join it using implode. Instead of user table mentioning, it'd be better to use u. and then user as u
-		$this->db->select('users.login, users.name, users.surname, users.sex, 
-			users.birthsday, users.tel, users.email, cars.manufacture, cars.model, 
+		$this->db->select('users.login, users.name, users.surname, users.sex,
+			users.birthsday, users.tel, users.email, cars.manufacture, cars.model,
 			users.user_created, users.avatar, authorization.user_rights, authorization.user_enabled');
 		$this->db->from('users');
 		$this->db->join('cars', 'users.avto_id = cars.id', 'left');
