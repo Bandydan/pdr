39d740d18448c3c345544a51b124c564929ca72f
Alexandr Kvak <a.kvak17@gmail.com>
2016-03-09 22:18:37 +0200

    add twig tamplates, some optimization, combined controllers in main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,14 +1,32 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Examples extends CI_Controller {
+class Main extends CI_Controller {
 
-    public function index($id = 0)
+    public function __construct()
     {
+        parent::__construct();
+        $this->load->model('examples_model');
         $this->load->library('twig');
+    }
+
+
+    //Start page
+    public function index()
+    {
+        $id = rand(1, 9);
+
+        $data['title'] = 'Garage - Удаление вмятин без покраски';
+        $data['examples'] = $this->examples_model->get_examples($id);
+        $data['cat'] = $this->config->item('categories');
+
+        echo $this->twig->render('start_view', $data);
+    }
+
+    //Example page
+    public function examples($id = 0)
+    {
 
         $data['title'] = 'Garage - Примеры работ';
-        $this->load->model('examples_model');
 
         if($id != 0) 
         {
@@ -18,24 +36,22 @@ class Examples extends CI_Controller {
         {
             $data['examples'] = $this->examples_model->get_examples_all();
         }
-        
+
         $data['cat'] = $this->config->item('categories');
 
         echo $this->twig->render('examples_view', $data);
     }
 
+    //all Examples page
     public function paginate($offset='') 
     { 
         $limit = 3;
         // $limit - кол-во запрашиваемых записей 
-        // $offset - смещение, т.е. с какой записи начинать выборку 
-        $this->load->library('twig');    
-        $this->load->model('examples_model'); 
+        // $offset - смещение, т.е. с какой записи начинать выборку  
 
         //$data['examples'] = $this->examples_model->get_examples_all();
             
         $data['examples'] = $this->examples_model->get_scrolling($limit,$offset); 
-         
         $data['cat'] = $this->config->item('categories');
 
         echo $this->twig->render('examples_view', $data);          
@@ -45,8 +61,7 @@ class Examples extends CI_Controller {
     {        
         $limit = 3;
         $offset = $_POST['startFrom'];       
-
-        $this->load->model('examples_model');            
+            
         $data['examples'] = $this->examples_model->get_scrolling($limit,$offset); 
 
         echo json_encode($data['examples']);
@@ -56,5 +71,23 @@ class Examples extends CI_Controller {
     public function config_arr()
     {
         echo json_encode($this->config->item('categories'));
+    }
+
+    //Education page
+    public function education()
+    {
+        $data['title'] = 'Обучение';
+        $data['cat'] = $this->config->item('categories');    
+        
+        echo $this->twig->render('education_view', $data);
+    }
+
+    //Contact page
+    public function contact()
+    {
+        $data['title'] = 'Garage - Контакты';
+        $data['cat'] = $this->config->item('categories');
+
+        echo $this->twig->render('contact_view', $data);
     }
 }
