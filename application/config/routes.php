a412ae3a01c2ec7853e3dedb35a9bc89d00bf0ce
Alona <alenija23@gmail.com>
2016-03-11 16:59:58 +0200

    moved the connect the script

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,9 +5,10 @@ $route['register'] = 'user/register';
 $route['login'] = 'user/login';
 $route['logout'] = 'user/logout';
 
-$route['default_controller'] = 'main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['default_controller'] = 'main';
 
 $route['(en|uk|ru)'] = $route['default_controller'];
 $route['(en|uk|ru)/(.+)'] = "$2";
