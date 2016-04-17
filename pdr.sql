a029b640be87f523d79726f43a3596b3f75ab970
skvak <a.kvak17@gmail.com>
2016-04-17 22:27:17 +0300

    some fix

--- pdr.sql
+++ pdr.sql
@@ -1,16 +1,3 @@
--- Valentina Studio --
--- MySQL dump --
--- ---------------------------------------------------------
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- ---------------------------------------------------------
-
-
 -- CREATE TABLE "Content" ----------------------------------
 CREATE TABLE `Content` ( 
 	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
@@ -1599,7 +1586,8 @@ delete from cars_mark where cars_mark.id=old.mark_id;
 end if;
 end;
 
-$$$ delimiter ;
+$$$ 
+delimiter ;
 -- ---------------------------------------------------------
 
 
@@ -1612,15 +1600,6 @@ begin
 delete from authorization where authorization.user_id=old.id;
 end;
 
-$$$ delimiter ;
+$$$ 
+delimiter ;
 -- ---------------------------------------------------------
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- ---------------------------------------------------------
-
-
